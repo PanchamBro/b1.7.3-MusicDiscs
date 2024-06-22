633dedac37a3f536db470b3ad793f207481eb999
PanchamBro <panchambro@proton.me>
2024-06-21 23:21:46 -0400

    Update Discs.java

--- src/main/java/jadestrouble/musicdiscs/items/Discs.java
+++ src/main/java/jadestrouble/musicdiscs/items/Discs.java
@@ -24,6 +24,11 @@ public class Discs {
     public static Item wait_Disc;
     public static Item pigstep_Disc;
     public static Item otherside_Disc;
+    public static Item five_Disc;
+    public static Item relic_Disc;
+    public static Item creator_Disc;
+    public static Item creatormusicbox_Disc;
+    public static Item precipice_Disc;
 @EventListener
 public void registerItems(ItemRegistryEvent event) {
   blocks_Disc = (new NewDiscs(Identifier.of(MOD_ID, "blocks_Disc"), "musicdiscs:blocks", "C418 - blocks")).setTranslationKey(MOD_ID, "Disc");
@@ -36,8 +41,13 @@ public void registerItems(ItemRegistryEvent event) {
   ward_Disc = (new NewDiscs(Identifier.of(MOD_ID, "ward_Disc"), "musicdiscs:ward", "C418 - ward")).setTranslationKey(MOD_ID, "Disc");
   eleven_Disc = (new NewDiscs(Identifier.of(MOD_ID, "eleven_Disc"), "musicdiscs:eleven", "C418 - 11")).setTranslationKey(MOD_ID, "Disc");
   wait_Disc = (new NewDiscs(Identifier.of(MOD_ID, "wait_Disc"), "musicdiscs:wait", "C418 - wait")).setTranslationKey(MOD_ID, "Disc");
-  pigstep_Disc = (new NewDiscs(Identifier.of(MOD_ID, "pigstep_Disc"), "musicdiscs:pigstep", "Lana Raine - Pigstep")).setTranslationKey(MOD_ID, "Disc");
-  otherside_Disc = (new NewDiscs(Identifier.of(MOD_ID, "otherside_Disc"), "musicdiscs:otherside", "Lana Raine - otherside")).setTranslationKey(MOD_ID, "Disc");
+  pigstep_Disc = (new NewDiscs(Identifier.of(MOD_ID, "pigstep_Disc"), "musicdiscs:pigstep", "Lena Raine - Pigstep")).setTranslationKey(MOD_ID, "Disc");
+  otherside_Disc = (new NewDiscs(Identifier.of(MOD_ID, "otherside_Disc"), "musicdiscs:otherside", "Lena Raine - otherside")).setTranslationKey(MOD_ID, "Disc");
+  five_Disc = (new NewDiscs(Identifier.of(MOD_ID, "five_Disc"), "musicdiscs:five", "Samuel Åberg - 5")).setTranslationKey(MOD_ID, "Disc");
+  relic_Disc = (new NewDiscs(Identifier.of(MOD_ID, "relic_Disc"), "musicdiscs:relic", "Aaron Cherof - Relic")).setTranslationKey(MOD_ID, "Disc");
+  creator_Disc = (new NewDiscs(Identifier.of(MOD_ID, "creator_Disc"), "musicdiscs:creator", "Lena Raine - Creator")).setTranslationKey(MOD_ID, "Disc");
+  creatormusicbox_Disc = (new NewDiscs(Identifier.of(MOD_ID, "creatormusicbox_Disc"), "musicdiscs:creatormusicbox", "Lena Raine - Creator (Music Box)")).setTranslationKey(MOD_ID, "Disc");
+  precipice_Disc = (new NewDiscs(Identifier.of(MOD_ID, "precipice_Disc"), "musicdiscs:precipice", "Aaron Cherof - Precipice")).setTranslationKey(MOD_ID, "Disc");
     items = new Item[]{
            Item.RECORD_THIRTEEN,
            Item.RECORD_CAT,
@@ -51,6 +61,11 @@ public void registerItems(ItemRegistryEvent event) {
            Discs.ward_Disc,
            Discs.wait_Disc,
            Discs.otherside_Disc,
+           Discs.five_Disc,
+           Discs.relic_Disc,
+           Discs.creator_Disc,
+           Discs.creatormusicbox_Disc,
+           Discs.precipice_Disc,
     };
    }
 }
